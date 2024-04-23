5d49bf2f2d14a2feb3bfceca4cb52d3a42abfebc
EC <enda@endacrossan.com>
2024-04-23 12:52:07 +0100

    Added complete and exit buttons.

--- gui.py
+++ gui.py
@@ -1,15 +1,22 @@
 import functions
 import PySimpleGUI as sg
 
+sg.theme('BlueMono')
 label = sg.Text("Please enter a todo item:")
-input_box = sg.InputText(tooltip="enter todo", key="todo")
+input_box = sg.InputText(tooltip="enter todo", key="todo", size=[31])
 add_button = sg.Button("Add")
 list_box = sg.Listbox(values=functions.get_todos(), key='todos',
-                      enable_events=True, size=[45, 10])
+                      enable_events=True, size=[30, 10])
 edit_button = sg. Button("Edit")
+complete_button = sg.Button("Complete")
+exit_button = sg.Button("Exit")
+
+layout = [[label], [input_box, add_button],
+          [list_box, edit_button, complete_button],
+          [exit_button]]
 
 window = sg.Window('To do app',
-                   layout=[[label], [input_box, add_button], [list_box, edit_button]],
+                   layout=layout,
                    font=('Century Gothic', 16))
 
 while True:
@@ -39,14 +46,23 @@ while True:
             todos[index] = new_todo + "\n"
             functions.write_todos(todos)
             window['todos'].update(values=todos)
-
+        case "Complete":
+            todo_to_complete = values['todos'][0]
+            print(todo_to_complete)
+            todos = functions.get_todos()
+            todos.remove(todo_to_complete)
+            functions.write_todos(todos)
+            window['todos'].update(values=todos)
+            window['todo'].update(value="")
         case 'todos':
             window['todo'].update(value=values['todos'][0])
-
+        case "Exit":
+            break
         case sg.WIN_CLOSED:
-            break   # break the while loop
+            # break   # break the while loop
+            exit()
 
 
 # window.read()  # display window
-
+print("Bye")
 window.close()
